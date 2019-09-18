cb5b6ea367eb95228b9881c86ff7df47895408d8
akosten <kosten27@gmail.com>
2019-09-18 15:53:37 +0300

    Added customize JSON for Post

--- grails-app/domain/task/Post.groovy
+++ grails-app/domain/task/Post.groovy
@@ -1,13 +1,16 @@
 package task
 
-import grails.rest.Resource
+import org.grails.datastore.gorm.GormEntity
 
-@Resource(uri = '/post', formats = ['json', 'xml'])
-class Post {
+//@Resource(uri = '/post', formats = ['json', 'xml'])
+class Post implements GormEntity<Post> {
 
     String message
     User user
 
     static constraints = {
     }
+
+
+
 }
--- grails-app/init/BootStrap.groovy
+++ grails-app/init/BootStrap.groovy
@@ -10,6 +10,7 @@ class BootStrap {
         def adminRole = new Role(authority: 'ROLE_ADMIN').save()
 
         def testUser = new User(username: 'me', password: 'password').save()
+        new User(username: 'test', password: 'test').save()
 
         UserRole.create testUser, adminRole
 
@@ -21,7 +22,7 @@ class BootStrap {
         new Post(message: "Message1", user: testUser).save()
         new Post(message: "Message2", user: testUser).save()
 
-        assert User.count() == 1
+        assert User.count() == 2
         assert Role.count() == 1
         assert UserRole.count() == 1
         assert Post.count() == 2
